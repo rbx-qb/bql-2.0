96e014b0053a4db12bc77a2c3402183ca857fc8f
RBX <rbx.qb@proton.me>
2025-06-24 10:40:37 -0300

    Update Console.tsx

--- frontend/components/Console.tsx
+++ frontend/components/Console.tsx
@@ -1,1 +1,11 @@
 
+const Console = () => {
+  return (
+    <div className="border p-4 mt-4">
+      <h2 className="font-semibold mb-2">Console Output</h2>
+      <pre className="bg-black text-green-300 p-2">Output will appear here</pre>
+    </div>
+  );
+};
+
+export default Console;
